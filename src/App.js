d94c6d96133be4ba63b636f83cb40a77393900c7
Jaewook <eari100@naver.com>
2021-09-21 22:11:47 +0900

    feat: axios를 이용하여 localhost:3000 접속 시 /api/v1/post/list API 호출되도록 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import React, { useEffect } from "react";
 import Table from './Table';
+import axios from "axios";
+
 
 function App() {
+  const baseUrl = "http://localhost:8080";
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  async function getTodos() {
+    await axios
+      .get(baseUrl + "/api/v1/post/list")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+
   return (
     <Table/>
   );
